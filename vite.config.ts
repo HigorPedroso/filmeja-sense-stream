1b03d69ca4fd01b0a83f5e6ab76213a7ae253b01
HigorPedroso <higor533@gmail.com>
2025-05-22 11:19:34 -0300

    feat: google alias

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        sitemap: path.resolve(__dirname, 'sitemap.xml')
-      }
-    }
   }
 });
