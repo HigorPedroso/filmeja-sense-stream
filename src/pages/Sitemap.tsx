48444f864509a4a5c4f424a68c7239e760230739
HigorPedroso <higor533@gmail.com>
2025-05-22 11:02:02 -0300

    feat: sitemaps

--- src/pages/Sitemap.tsx
+++ src/pages/Sitemap.tsx
@@ -11,19 +11,13 @@ export const Sitemap = () => {
           .eq('status', 'published');
 
         const baseUrl = window.location.origin;
-        const xmlString = `<?xml version="1.0" encoding="UTF-8"?>
+        const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${baseUrl}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/blog</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.8</priority>
   </url>${posts?.map(post => `
   <url>
     <loc>${baseUrl}/blog/${post.slug}</loc>
@@ -33,22 +27,9 @@ export const Sitemap = () => {
   </url>`).join('')}
 </urlset>`;
 
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(xmlString, 'application/xml');
-        const serializer = new XMLSerializer();
-        const finalXml = serializer.serializeToString(xmlDoc);
-
-        const blob = new Blob([finalXml], { type: 'application/xml' });
+        const blob = new Blob([xmlContent], { type: 'application/xml' });
         const url = URL.createObjectURL(blob);
-        
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'sitemap.xml';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        URL.revokeObjectURL(url);
-
+        window.location.href = url;
       } catch (error) {
         console.error('Error generating sitemap:', error);
       }
