a2e0ff0a1254fabc7b23cb81ab7f1708ce3f5b55
HigorPedroso <higor533@gmail.com>
2025-05-22 11:45:48 -0300

    feat: now vai

---
+++ supabase/functions/robots/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+
+serve(async () => {
+  const robotsTxt = `User-agent: *
+Allow: /
+
+# Sitemaps
+Sitemap: https://filmeja.com/sitemap.xml`;
+
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'public, max-age=3600'
+    }
+  });
+});
